9d8c06e54ca1925c9c6f8823584c18a2057815a2
Miłosz <marzec1310@gmail.com>
2025-06-04 20:25:23 +0200

    Added Bean, tests passes

--- src/main/java/pl/wsb/fitnesstracker/mail/internal/MailConfig.java
+++ src/main/java/pl/wsb/fitnesstracker/mail/internal/MailConfig.java
@@ -1,10 +1,18 @@
 package pl.wsb.fitnesstracker.mail.internal;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 @EnableConfigurationProperties(MailProperties.class)
 class MailConfig {
 
-}
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+        }
+    }
+
